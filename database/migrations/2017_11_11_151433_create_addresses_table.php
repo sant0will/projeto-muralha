ec20cac6665cc1639c69f91b481e857766734230
Willian <awillian0@gmail.com>
2017-11-12 15:34:14 -0200

    Alterações das migrations e modelo standard

--- database/migrations/2017_11_11_151433_create_addresses_table.php
+++ database/migrations/2017_11_11_151433_create_addresses_table.php
@@ -15,12 +15,17 @@ class CreateAddressesTable extends Migration
     {
         Schema::create('addresses', function (Blueprint $table) {
             $table->increments('id');
+            $table->integer('profile_id')->unsigned();
             $table->foreign('profile_id')->references('id')->on('profiles');
-            
-
-
-
-
+            $table->string('rua', 200);
+            $table->integer('numero');
+            $table->integer('cep');
+            $table->string('bairro', 100);
+            $table->string('complemento', 500);
+            $table->string('tipo', 45);
+            $table->string('cidade', 45);
+            $table->string('estado', 45);
+            $table->string('pais', 45);
             $table->timestamps();
         });
     }
